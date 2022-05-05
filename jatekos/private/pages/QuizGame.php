3a1257b75d03ed0c0b10f920df2c4c2db0061b54
safarcsikViktor <85190682+safarcsikViktor@users.noreply.github.com>
2022-05-05 16:08:13 +0200

    Create QuizGame

---
+++ jatekos/private/pages/QuizGame.php
@@ -0,0 +1,21 @@
+<link href="QuizGame.css" rel="stylesheet">
+<script defer src="script.js"></script>
+
+<title>Kviz App</title>
+<div class="container">
+    <div id="question-container" class="hide">
+        <div id="question">Kvíz</div>
+        <div id="question">Mikor volt a Szabadságharc?</div>
+        <div id="answer-buttons" class="btn-grid">
+            <button class=""btn>1848</button>
+            <button class=""btn>1567</button>
+            <button class=""btn>1222</button>
+            <button class=""btn>1867</button>
+         </div>
+    </div>
+    <div class="controls">
+        <button id="start-btn" class="start-btn btn">Start</button>  
+        <button id="next-btn" class="next-btn btn">Next</button>
+
+        </div>
+</div>
